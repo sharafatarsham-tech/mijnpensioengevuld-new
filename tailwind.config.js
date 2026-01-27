d70ba507d8f8c1765551da9eb5bfac784f6a9829
Arsham Sharafat <sharafatarsham@gmail.com>
2026-01-27 23:41:17 +0100

    Revert tailwind.config.js - remove Relume preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require("@relume_io/relume-tailwind")],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
