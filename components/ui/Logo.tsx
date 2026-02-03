dea8230135456eaaa9ba62e73317b57d4ec66491
Arsham Sharafat <sharafatarsham@gmail.com>
2026-02-03 01:21:38 +0100

    Logo: pig logo groter gemaakt (h-16 w-64 op desktop)

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -25,10 +25,10 @@ export function Logo({ variant = "dark" }: { variant?: "dark" | "light" }) {
     );
   }
 
-  // Voor lichte achtergronden: volledig logo (2.5x groter)
+  // Voor lichte achtergronden: volledig logo - groter
   return (
     <Link href="/" className="block group">
-      <div className="relative h-12 w-40 sm:h-14 sm:w-48 lg:h-20 lg:w-72">
+      <div className="relative h-14 w-48 sm:h-16 sm:w-56 lg:h-16 lg:w-64">
         <Image
           src="/logo-mijnpensioen.png"
           alt="MijnPensioenGevuld.nl"
